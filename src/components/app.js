ee18b644de9b838c5ab6edc13b0fc3ffa89d4708
nivonne <nimera27@gmail.com>
2020-05-16 23:28:40 -0500

    Updated

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import JournalList from "./journals/journal_list";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>Rect, Props and state deep dive</h1>
-        <h2>React Redux Router</h2>
+      <div className="app">
+        <h1>React, Props and state deep dive</h1>
+        
+        <JournalList heading="List 1" />
+        <JournalList heading="List 2" />
       </div>
     );
   }
